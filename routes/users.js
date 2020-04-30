6959383898f7ed26edb425deb15f40ac8ab8aff8
Xianhai Cao <xianhaic@gmail.com>
2020-04-29 17:18:56 -0700

    Added route for creating connections
    This allows a new clothing instance to be associated with a user. IE, a
    user adds a certain clothing to their profile. Requires both the user
    and the clothing to exist, otherwise this precedure fails.

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) => {
   helper.getItem(docClient, res, req.params.id)
 })
 
-// [GET]: retrieves user id given the accoutn id
+// [GET]: retrieves user id given the account id
 router.get('/acc/:acc_id', (req, res) => {
   console.log(req.params)
   const params = {
@@ -83,7 +83,7 @@ router.get('/:id/clothes', (req, res) => {
 // NOTE: using an existing id will overwrite the previous item
 router.post('/new', (req, res) => {
   // validate parameters
-  if (!validate(req.body)) {
+  if (!validateUser(req.body)) {
     res.status(400).send('Bad arguments')
     return
   }
@@ -101,9 +101,81 @@ router.post('/new', (req, res) => {
   helper.putItem(docClient, res, params)
 })
 
-const validate = (data) => {
+router.post('/:id/clothes/new', async (req, res) => {
+  if (!validateConn(req.body)) {
+    res.status(400).send('Bad arguments')
+    return
+  }
+
+  try {
+    var params = {
+      TableName: 'Profile',
+      Key: {
+        'PK': req.params.id,
+        'SK': 'meta',
+      }
+    }
+
+    // check if user exists
+    var user = await docClient.get(params).promise()
+    if (Object.keys(user).length === 0) {
+      console.error('Item not found:', params.Item.PK)
+      res.status(400).send('Unable to create connection: user does not exist')
+      return
+    }
+    user = user.Item
+
+    params = {
+      TableName: 'Profile',
+      Key: {
+        'PK': req.body.clothes_id,
+        'SK': 'meta',
+      }
+    }
+
+    // check if clothing exists
+    var clothes = await docClient.get(params).promise()
+    if (Object.keys(clothes).length === 0) {
+      console.error('Item not found:', params.Item.PK)
+      res.status(400).send('Unable to create connection: clothing does not exist')
+      return
+    }
+    clothes = clothes.Item
+
+    // create connection
+    params = {
+      TableName: 'Profile',
+      Item: {
+        'PK': clothes.PK,
+        'SK': 'conn_'.concat(uuid()),
+        'User_GSI': user.PK,
+        'time': req.body.time.toString(),
+        'size': req.body.size.toString(),
+        'cost': req.body.cost.toString(),
+      }
+    }
+
+    helper.putItem(docClient, res, params)
+    return
+
+  } catch (err) {
+    console.error('Unable to get item. Error JSON:', JSON.stringify(err, null, 2))
+    res.status(500).send('Unable to create connection')
+  }
+})
+
+const validateUser = (data) => {
   if (isEmpty(data.acc_id)) return false
   if (isEmpty(data.name)) return false
+
+  return true
+}
+
+const validateConn = (data) => {
+  if (isEmpty(data.clothes_id)) return false
+  if (isEmpty(data.time)) return false
+  if (isEmpty(data.size)) return false
+  if (isEmpty(data.cost)) return false
 
   return true
 }
