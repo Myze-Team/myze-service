7d3541d7b4a6791b45dfb3924bc43c1848de2f10
Paul Pan <panjunhong05@gmail.com>
2020-07-11 13:45:47 -0700

    Removed extra keys

--- myze_service/db.py
+++ myze_service/db.py
@@ -3,7 +3,7 @@ import click
 from flask.cli import with_appcontext
 
 def get_db():
-    return boto3.resource('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2', aws_access_key_id='test', aws_secret_access_key='test')
+    return boto3.client('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2', aws_access_key_id='test', aws_secret_access_key='test')
 
 def delete_tables(dynamodb):
     tables = dynamodb.list_tables()['TableNames']
@@ -26,14 +26,6 @@ def create_tables(dynamodb):
                 'AttributeName': 'id',
                 'AttributeType': 'S'
             },
-            {
-                'AttributeName': 'email',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'password',
-                'AttributeType': 'S'
-            }
         ],
         ProvisionedThroughput={
             'ReadCapacityUnits': 5,
