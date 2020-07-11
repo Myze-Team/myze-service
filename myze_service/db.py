89c275f3d156115b4caf46b2d5fb7364a45a1db5
Paul Pan <panjunhong05@gmail.com>
2020-07-10 21:14:47 -0700

    Database update

---
+++ myze_service/db.py
@@ -0,0 +1,43 @@
+import boto3
+import click
+from flask.cli import with_appcontext
+
+def get_db():
+    return boto3.client('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2')
+
+def delete_tables(dynamodb):
+    tables = dynamodb.list_tables()['TableNames']
+    for table in tables:
+        dynamodb.delete_table(TableName=table)
+
+def create_tables(dynamodb):
+    table = dynamodb.create_table(
+        TableName='Profiles',
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    dynamodb = get_db()
+    delete_tables(dynamodb)
+    create_tables(dynamodb)
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
