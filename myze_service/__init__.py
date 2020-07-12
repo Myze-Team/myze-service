7a4ff303965f521861fd30ae8b3b508d82373dfb
unknown <maxwellwong91@gmail.com>
2020-07-11 21:40:53 -0700

    tried some import

--- myze_service/__init__.py
+++ myze_service/__init__.py
@@ -37,7 +37,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    #import routes.helloWorld
+    import routes.helloWorld
 
     #app.register_blueprint(routes.helloWorld)
 
